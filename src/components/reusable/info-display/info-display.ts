72230aa1167e527e34235e8733ca7f56349a011b
Sebbb1 <guyonnaud.sebastien@gmail.com>
2020-05-05 13:56:33 +0200

    removed InfoDisplay component

--- src/components/reusable/info-display/info-display.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export const Info = styled('div')`
-    background-color: white;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
-    text-align: center;
-    color: black;
-`;
