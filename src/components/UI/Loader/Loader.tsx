5d46b59d5efa23511d5b56a003b79a4ddd3a258b
Sebbb1 <guyonnaud.sebastien@gmail.com>
2020-05-16 13:12:44 +0200

    loader spinner component

---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import styles from './Loader.module.css';
+
+const Loader: FunctionComponent = () => {
+    return (
+        <div className={styles.loader} />
+    )
+}
+
+export default Loader;
