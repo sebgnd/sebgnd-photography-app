93d04a030edcf28d1cde2255acbe6315f12e7a8c
Sebbb1 <guyonnaud.sebastien@gmail.com>
2020-05-19 12:06:25 +0200

    added thumbnailId in Category class and getAll service function

--- src/helper/category/Category.ts
+++ src/helper/category/Category.ts
@@ -1,19 +1,42 @@
+const THUMBNAIL_TYPE = ['thumbnail_medium', 'thumbnail_small'];
+
 export default class Category {
     private _id: string;
     private _displayName: string;
+    private _thumbnailId: number;
 
-    constructor(id: string = '', displayName: string = '') {
+    constructor(id: string = '', displayName: string = '', thumbnailId: number = -1) {
         this._id = id;
         this._displayName = displayName;
+        this._thumbnailId = thumbnailId;
     }
 
     static format(json: any): Category {
         const { id, displayName } = json;
-        return new Category(id, displayName);
+        let thumbnailId = -1;
+        if (json.thumbnail.id) {
+            thumbnailId = json.thumbnail.id;
+        }
+        const category = new Category(id, displayName, thumbnailId);
+        return category;
+    }
+
+    getThumbnailUrl(type: string): string {
+        if (this._thumbnailId !== -1) {
+            if (THUMBNAIL_TYPE.includes(type)) {
+                return `http://localhost:8000/image/${this.id}/${type}/${this.thumbnailId}`;
+            }
+            return `http://localhost:8000/image/${this.id}/thumbnail_medium/${this.thumbnailId}`;
+        }
+        return 'https://via.placeholder.com/450';
     }
 
     clone(): Category {
         return { ...this };
+    }
+
+    get thumbnailId() {
+        return this._thumbnailId;
     }
 
     get id() {
--- src/helper/category/CategoryService.ts
+++ src/helper/category/CategoryService.ts
@@ -11,4 +11,24 @@ export default class CategoryService {
             throw e;
         } 
     }
+
+    async getK(k: number): Promise<Category[]> {
+        try {
+            const data = await HttpRequest.getData(`http://localhost:8000/categories/limit/${k}`);
+            return data.map((category: any) => Category.format(category));
+
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getAll(): Promise<Category[]> {
+        try {
+            const data = await HttpRequest.getData('http://localhost:8000/categories');
+            return data.map((cateogory: any) => Category.format(cateogory));
+        
+        } catch (e) {
+            throw e;
+        }
+    }
 }
